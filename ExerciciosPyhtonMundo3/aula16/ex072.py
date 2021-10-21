b19d78ab14777c1447bfb67d347caaa7e3b8f448
Jameson Santos <jamesonbezerrasantos@gmail.com>
2021-10-20 22:42:29 -0300

    At

--- ExerciciosPyhtonMundo3/aula16/ex072.py
+++ ExerciciosPyhtonMundo3/aula16/ex072.py
@@ -6,7 +6,25 @@ extensos = ('zero','um','dois','três','quatro','cinco','sei','sete','oito','nov
            'dez','onze','doze','treze','quatorze','quinze','dezesseis','dezessete',
            'dezoito','dezenove','vinte',)
 
-numero = int(input('Digite um número entre 0 e 20: '))
+''' numero = int(input('Digite um número entre 0 e 20: '))
 while numero not in range(0, 21):
     numero = int(input('[ERRO] Digite um número entre 0 e 20: '))
-print(f'Você digitou o número {extensos[numero]}')
+print(f'Você digitou o número {extensos[numero]}') '''
+
+while True:
+    numero = int(input('Digite um número entre 0 e 20: '))
+    if numero < 0 or numero > 20:
+        print('Tente novamente. ', end='')
+        continue
+    if 0 <= numero <= 20:
+        print(f'Você digitou o número {extensos[numero]}')
+        continuar = str(input('Deseja continuar? [S/N]')).strip().upper()[0]
+        if continuar in 'Ss':
+            continue
+        else:
+            print('PROGRAMA ENCERRADO.')
+            break
+
+
+
+
